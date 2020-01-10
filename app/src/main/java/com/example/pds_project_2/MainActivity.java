4471d9b95bf143731f4225b4f861420bdc56a449
jfamousket <jfamousket@gmail.com>
2020-01-10 17:57:52 +0100

    setup rabbitmq

--- app/src/main/java/com/example/pds_project_2/MainActivity.java
+++ app/src/main/java/com/example/pds_project_2/MainActivity.java
@@ -9,13 +9,43 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.pds_project_2.ui.main.SectionsPagerAdapter;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class MainActivity extends AppCompatActivity {
+    private BlockingDeque<String> queue = new LinkedBlockingDeque<>();
+    ConnectionFactory factory = new ConnectionFactory();
+    Thread subscribeThread;
+    Thread publishThread;
+
+    void publishMessage(String message) {
+        try {
+            Log.d("", "[q] " + message);
+            queue.putLast(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setupConnectionFactory() {
+        String uri = "amqp://guest:guest@192.168.0.109:15672";
+        factory.setAutomaticRecoveryEnabled(false);
+        factory.setUsername("famous");
+        factory.setPassword("famous");
+        factory.setHost("10.222.2.16");
+        factory.setPort(5672);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
